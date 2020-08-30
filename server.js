6887827138923da84c5f79be9f7532c8ca5d4ae8
Arian Rahbar <arahbar@usc.edu>
2020-08-30 02:38:09 -0500

    eh

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ if (process.env.NODE_ENV !== 'production') {
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(morgan('dev'))
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(morgan('dev'))
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
